4863269457d49c0080458705fc99e4e46f4649af
Zoltig <T.koltys@wp.pl>
2025-10-18 09:54:25 +0200

    Init: Maven skeleton

---
+++ coffeecraft/src/main/java/pl/pollub/coffeecraft/App.java
@@ -0,0 +1,13 @@
+package pl.pollub.coffeecraft;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
